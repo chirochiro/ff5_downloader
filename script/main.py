66d8411775d0759866238fd5c170f0f0e12384be
小嶋康暉 <kojimadamsmith@kojimayasushiakiranoMacBook-Air.local>
2025-07-13 13:14:02 +0900

    create main.py

--- script/main.py
+++ script/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from fitst-test!")
+# script/main.py
 
+import sys
+import os
+
+# src ディレクトリをモジュール検索パスに追加
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
+from download_ff5 import download_ff5_monthly
 
 if __name__ == "__main__":
-    main()
+    save_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", "FamaFrench5Factors_monthly.csv"))
+    download_ff5_monthly(save_path)
+    print("Download and processing complete.")
